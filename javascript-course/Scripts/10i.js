d61a578498e3ed9f09e17986ccad95def35114bf
JesperMyhre <jesper.ovreness@gmail.com>
2023-10-12 14:35:40 +0200

    fbnWSEIPGBSPDJKFB

--- javascript-course/Scripts/10i.js
+++ javascript-course/Scripts/10i.js
@@ -1,18 +1,28 @@
 let calculation = localStorage.getItem('calculation') || "";
+let history = localStorage.getItem('history') || "";
 displayCalculation();
+
 
 function updateCalculation(value) {
     calculation += value;
     console.log(calculation);
-    saveCalculation();
+
+    history += calculation;
+
     displayCalculation();
+    saveCalculation();
 }
 
 function saveCalculation() {
     localStorage.setItem('calculation', calculation);
+    localStorage.setItem('history', history);
     displayCalculation();
 }
 
 function displayCalculation() {
   document.querySelector('.js-calculation').innerHTML = calculation;
+}
+
+function displayHistory() {
+  alert(history);
 }
